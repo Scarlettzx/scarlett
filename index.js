feefbad49bdd86d441fedc1f079b85c34ca4a595
Scarlettzx <scarlettz.mew@gmail.com>
2023-01-07 21:04:37 +0700

    Lec7 add all files of Project
    Lec7 Git 💕

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const chalk = require('Chalk');
+const debug = require('debug')('index');
+
+app.use(morgan('dev'))
+app.get("/", (req,res)=>{
+    res.send('Hello World');
+})
+
+const port = 3000;
+app.listen(port, ()=>{
+    debug(`Server is running on Port ${chalk.blue(port)}`) 
+})
